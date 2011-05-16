51c040b889f0d4ddb40eacd4f1feb8afa9e72848
Tuomas Tynkkynen <dezgeg@gmail.com>
2011-05-16 14:50:52 +0300

    Eka commit, tyhjä NetBeansin python-projekti

---
+++ src/geovision.py
@@ -0,0 +1,8 @@
+# To change this template, choose Tools | Templates
+# and open the template in the editor.
+
+__author__="tmtynkky"
+__date__ ="$16.5.2011 14:12:12$"
+
+if __name__ == "__main__":
+    print "Hello World"
