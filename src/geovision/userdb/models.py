3308aac8061409cd71cfb6c6449d0a22370d83c6
Tuomas Tynkkynen <dezgeg@gmail.com>
2011-05-25 14:50:32 +0300

    Muokattu alustava käyttäjädb vastaamaan asiakkaan vaatimuksia.

--- src/geovision/userdb/models.py
+++ src/geovision/userdb/models.py
@@ -1,12 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-
-class Collection(models.Model):
-	name = models.CharField(max_length=32)
-	description = models.TextField()
-	users = models.ManyToManyField(User)
-
 class Sample(models.Model):
 	sample_id = models.CharField(max_length=32)
-	collection = models.ForeignKey(Collection)
+	owner = models.ForeignKey(User, related_name='samples')
+
+class Collection(models.Model):
+	name = models.CharField(max_length=64)
+	description = models.TextField()
+	users = models.ManyToManyField(User, related_name='collections')
+	samples = models.ManyToManyField(Sample, related_name='collections')
+	owner = models.ForeignKey(User, related_name='own_collections')
