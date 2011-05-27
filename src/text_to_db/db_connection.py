a44cd4e04573165268f9e8bc644a00970f93133a
Lasse Tyrväinen <sundokan@gmail.com>
2011-05-27 12:29:24 +0300

    Commented out unused db code

--- src/text_to_db/db_connection.py
+++ src/text_to_db/db_connection.py
@@ -7,13 +7,20 @@ __date__ = "$May 24, 2011 3:33:22 PM$"
 import os
 import psycopg2
 
-def initiate_connection(database_name=os.getlogin(), username=os.getlogin(), password=None):
-	if username is 'tkt_gvis':
-		passwordfile = open(os.environ['HOME'] + '/.psql_password')
-		password = passwordfile.readline().strip()
-	connection_information = ("dbname=" + database_name
-							  + "user=" + username
-							  + "password=" + password)
+# TODO: muuta käyttämään djangon ORBia
 
-	db_connection = psycopg2.connect(connection_information)
-	return db_connection
+class DbConnection:
+	def initiate_connection(database_name=os.getlogin(), username=os.getlogin(), password=None):
+		if username is 'tkt_gvis':
+			__passwordfile = open(os.environ['HOME'] + '/.psql_password')
+			password = __passwordfile.readline().strip()
+		if password is not None:
+			self.connection_information = ("dbname=" + database_name
+								  + "user=" + username
+								  + "password=" + password)
+		else:
+			self.connection_information = ("dbname=" + database_name
+								  + "user=" + username)
+
+		db_connection = psycopg2.connect(connection_information)
+		return db_connection
