d30166bfc8a33a1efc5b01086f8be08e7cb50f40
jjlaukka <jjlaukka@tktl-2009>
2011-06-02 10:29:52 +0300

    create_json structure designed

--- src/geovision/text_to_db/create_JSON.py
+++ src/geovision/text_to_db/create_JSON.py
@@ -6,27 +6,43 @@ __date__ ="$Jun 1, 2011 1:56:40 PM$"
 
 import os
 os.environ['DJANGO_SETTINGS_MODULE'] = 'geovision.settings'
-from geovision.viz.models import Blast, Read, DbEntry, Results
+from geovision.viz.models import Blast, Read, DbEntry, Result
+
+results = ""
+depth_limit = 4
 
 def get_Results(ecnumber):
-    objects = Results.objects.filter(ec_number=ecnumber)
+    objects = Result.objects.filter(ec_number = ecnumber)
     return objects
 
-def create_json(self, ecnumber):
-    center_nodes = get_Results(ecnumber)
-    db_children= get_db_children(center_nodes)
+def create_json(ecnumber):
+    zero_nodes = get_Results(ecnumber)
+    json_file = open("/home/jjlaukka/Opiskelu/ohtuprojekti/Jit/Examples/RGraph/json_file.json", 'w')
 
+    json_file.write("{\n")
 
-def get_db_children(nodes):
-    results = []
-    for obj in nodes:
-        results.append(DbEnty.objects.get)
+    json_file.write("id: \"" + ecnumber + "\",\n")
+    json_file.write("name: \"" + ecnumber + "\",\n")
+    json_file.write("children: [" + getChildren(zero_nodes) + "]\n")
 
-def get_read_children(self, db):
-    return Read.objects.filter()
+    json_file.write("}")
 
-def get_ec_children(self, db):
-    return Results.objects.filter(db_entry.r)
+def get_children(ec_nodes):
+    
+    get_children.depth_counter += 1
+
+    for obj in ec_nodes:
+        result = result + "\t{\n\tid: \"" + obj.db_entry.read_id + "\",\n"
+        result = result + "\tname: \"" + obj.db_entry.read_id + "\",\n"
+        result = result + "\tdata: {\n\t\trelation: \"<h4>relations here..</h4>\"\n\t},\n"
+
+        if get_children.depth_counter < depth_limit:
+            read_children = Result.objects.filter(db_entry=obj.db_entry)
+        else:
+            results = results + "\tchildren: []\n\t}"
+            get_children.depth_counter -= 1
+            return children;
+
 
 if __name__ == "__main__":
-    create_json(ecnumber)
+    create_json("1.2.3.22")
