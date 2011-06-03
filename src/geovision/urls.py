0aef67c4f72dc4228838e40d1a76356980cbc827
tulilaulu <tulilaulu@gmail.com>
2011-06-03 13:59:03 +0300

    admin fixed

--- src/geovision/urls.py
+++ src/geovision/urls.py
@@ -12,11 +12,11 @@ urlpatterns = patterns('',
     (r'^register$', 'userdb.views.register'),
     (r'^registering$', 'userdb.views.registering'),
     #(r'^polls/(?P<poll_id>\d+)/vote/$', 'polls.views.vote'),
-    (r'', 'userdb.views.login'), #everything else regirects to login for now
     
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
+    (r'', 'userdb.views.login'), #everything else regirects to login for now
 )
