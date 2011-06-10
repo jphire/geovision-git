c7e2c0256fbc712db106a0d5d70aae5277034c65
Janne Laukkanen <janne.laukkanen83@gmail.com>
2011-06-10 09:47:04 +0300

    repaired to use bulk inserter

--- src/geovision/text_to_db/blast_parser.py
+++ src/geovision/text_to_db/blast_parser.py
@@ -9,20 +9,24 @@ import os
 os.environ['DJANGO_SETTINGS_MODULE'] = 'geovision.settings'
 from geovision.viz.models import Blast
 from geovision.viz.models import Read, DbEntry
+from geovision.text_to_db.bulk_inserter import BulkInserter
 
 def create_blast(db_name, sample_name, filename):
     filehandle = open(filename, 'r')
+
+    inserter = BulkInserter(Result)
 
     for line in filehandle:
             (read_id, db_seq_id, pident, length, mismatch, gapopen, qstart, qend, sstart, send, error_value, bitscore) = line.split(None)
             read = Read.objects.get(read_id = read_id, sample = sample_name)
             db_entry = DbEntry.objects.get(read_id = db_seq_id, source_file = db_name)
-            Blast.objects.create(read = read,  database_name = db_name, db_entry = db_entry, pident = pident,
+            inserter.save(Result(read = read,  database_name = db_name, db_entry = db_entry, pident = pident,
                 length = length, mismatch = mismatch, gapopen = gapopen, qstart = qstart,
-                qend = qend, sstart = sstart, send = send, error_value = error_value, bitscore = bitscore)
-
+                qend = qend, sstart = sstart, send = send, error_value = error_value, bitscore = bitscore))
+    inserter.close()
+    
 if __name__ == '__main__':
     import sys
     filename = sys.argv[1]
-    (sample_name, db_name) = filename.split('.')
+    (sample_name, db_name) = os.path.basename(filename).split('.')
     create_blast(db_name, sample_name, filename)
