d7f6ea34adaeec746888b53630556ab46a44d4ef
tulilaulu <tulilaulu@gmail.com>
2011-06-13 12:26:55 +0300

    easier to find the admin-site

--- src/geovision/urls.py
+++ src/geovision/urls.py
@@ -20,6 +20,6 @@ urlpatterns = patterns('',
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin', include(admin.site.urls)),
     (r'', 'userdb.views.loginpage'), #everything else regirects to login for now
 )
