78567272220e5f02f112b0a1161bf02922260ba9
tulilaulu <tulilaulu@gmail.com>
2011-06-15 10:22:38 +0300

    comments

--- src/geovision/urls.py
+++ src/geovision/urls.py
@@ -21,5 +21,6 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     url(r'^admin', include(admin.site.urls)),
+    
     (r'', 'userdb.views.loginpage'), #everything else regirects to login for now
 )
