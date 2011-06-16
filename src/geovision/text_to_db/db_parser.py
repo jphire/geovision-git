b5b6b4af01774b7794e63f3d87ce608d617f8dfc
Lasse Tyrväinen <sundokan@gmail.com>
2011-06-16 16:55:53 +0300

    Added more uniprot data fields to db model, modified db parser to match

--- src/geovision/text_to_db/db_parser.py
+++ src/geovision/text_to_db/db_parser.py
@@ -22,8 +22,12 @@ class DbfileParser:
 		self.infoline = []
 		self.id = ''
 		self.description = ''
+		# Uniprot-specific fields
+		self.sub_db = ''
+		self.entry_name = ''
 		self.os_field = ''
 		self.other_info = ''
+		#
 		self.dbname = ''
 		if self.source.find("uniprot") != -1:
 			self.dbname = "uniprot"
@@ -34,7 +38,7 @@ class DbfileParser:
 		"""
 		Function for reading database fasta file entries one at a time.
 		Parsing is slightly different for different databases.
-		Uniprot: OS= field is parsed separately
+		Uniprot: OS= field is parsed separately, id field is split into components
 		Frnadb: lcl| prefix in ID fields is removed
 		"""
 		if len(self.nextline) is 0:
@@ -43,15 +47,26 @@ class DbfileParser:
 		self.infoline = self.nextline.strip().strip('>').split(None, 1)
 		self.id = self.infoline[0]
 		if self.dbname is "uniprot":
+			split_id = self.id.split("|")
+			self.id = split_id[1]
+			self.sub_db = split_id[0]
+			self.entry_name = split_id[2]
 			self.infoline = self.infoline[1].split('OS=')
 			self.description = self.infoline[0].strip()
 			next_pattern_start = re.search(re.compile('[A-Z]{2}='), self.infoline[1]).start()
 			self.os_field = self.infoline[1][:next_pattern_start-1]
 			self.other_info = self.infoline[1][next_pattern_start:]
 		elif self.dbname is "frnadb":
+			self.description = self.infoline[1]
 			self.id = self.id.split('|')[1]
+			self.sub_db = ''
+			self.entry_name = ''
+			self.os_field = ''
+			self.other_info = ''
 		else:
 			self.description = self.infoline[1]
+			self.sub_db = ''
+			self.entry_name = ''
 			self.os_field = ''
 			self.other_info = ''
 		self.nextline = self.textfile.readline()
@@ -63,5 +78,5 @@ class DbfileParser:
 				break
 
 		return DbEntryModel(source_file = self.source, db_id = self.id,
-				description = self.description, os_field = self.os_field,
-				other_info = self.other_info, data = self.dnadata)
+				description = self.description, sub_db = self.sub_db, entry_name = self.entry_name,
+				os_field = self.os_field, other_info = self.other_info, data = self.dnadata)
--- src/geovision/viz/models.py
+++ src/geovision/viz/models.py
@@ -6,16 +6,27 @@ class Read(models.Model):
 	read_id = models.CharField(max_length=64)
 	description = models.TextField()
 	data = models.TextField()
+	class Meta:
+		unique_together = ("sample", "read_id")
 
 class DbEntry(models.Model):
 	source_file = models.CharField(max_length=32)
-	read_id = models.CharField(max_length=64)
+	db_id = models.CharField(max_length=32, primary_key=True)
 	description = models.TextField()
 	data = models.TextField()
+	# Uniprot-specific fields below
+	sub_db = models.CharField(max_length=4, blank=True)
+	entry_name = models.CharField(max_length=16, blank=True)
+	os_field = models.CharField(max_length=128, blank=True)
+	other_info = models.TextField(blank=True)
 
+class DbUniprotEcs(models.Model):
+	db_id = models.ForeignKey(DbEntry)
+	protein_existence_type = models.CharField(max_length=4)
+	ecs = models.CharField(max_length=32)
 
 class Blast(models.Model): # qseqid sseqid pident length mismatch gapopen qstart qend sstart send evalue bitscore
-	read = models.ForeignKey(Read)
+	read = models.CharField(max_length=64)
 	database_name = models.CharField(max_length=16)
 	db_entry = models.ForeignKey(DbEntry)
 	pident = models.DecimalField(max_digits=4,decimal_places=2)
