1b8c1c7e5ce1d7d8c065977d9de2728a08c27971
Tuomas Tynkkynen <dezgeg@gmail.com>
2011-06-24 13:13:36 +0300

    Fixed ecs parser tests.

--- src/geovision/text_to_db/test_uniprot_ecs_parser.py
+++ src/geovision/text_to_db/test_uniprot_ecs_parser.py
@@ -14,18 +14,26 @@ TEST_FILE_PATH = PROJECT_PATH + '/text_to_db/testfiles/'
 class Test_Uniprot_Ecs_parser(unittest.TestCase):
 	def test_ecs_parse_first(self):
 		parser = uniprot_ecs_parser.EcsFileParser(TEST_FILE_PATH + "test_uniprot.ecs")
-		entry = parser.next_ecs_entry()
+		entries = list(parser.next_ecs_entry())
+		self.assertEqual(len(entries), 1)
+		entry = entries[0]
 		self.assertEqual(entry.db_id, 'Q91G55')
 		self.assertEqual(entry.protein_existence_type, 'P')
-		self.assertEqual(entry.ecs, '?')
+		self.assertEqual(entry.ec, '?')
 
 	def test_ecs_parse_second(self):
 		parser = uniprot_ecs_parser.EcsFileParser(TEST_FILE_PATH + "test_uniprot.ecs")
 		parser.next_ecs_entry()
-		entry = parser.next_ecs_entry()
-		self.assertEqual(entry.db_id, 'P27365')
-		self.assertEqual(entry.protein_existence_type, 'ET')
-		self.assertEqual(entry.ecs, '5.3.3.1,1.1.1.145')
+		entries = list(parser.next_ecs_entry())
+		self.assertEqual(len(entries), 2)
+		entry1 = entries[0]
+		self.assertEqual(entry1.db_id, 'P27365')
+		self.assertEqual(entry1.protein_existence_type, 'ET')
+		self.assertEqual(entry1.ec, '5.3.3.1')
+		entry2 = entries[1]
+		self.assertEqual(entry2.db_id, 'P27365')
+		self.assertEqual(entry2.protein_existence_type, 'ET')
+		self.assertEqual(entry2.ec, '1.1.1.145')
 
 	def test_ecs_parse_past_the_end(self):
 		parser = uniprot_ecs_parser.EcsFileParser(TEST_FILE_PATH + "test_uniprot.ecs")
