c8066c70cf293973d6def806097fcffca22af394
Tuomas Tynkkynen <dezgeg@gmail.com>
2011-06-24 14:31:42 +0300

    Refactored a bit + implemented search by enzyme name.

--- src/geovision/viz/views.py
+++ src/geovision/viz/views.py
@@ -24,40 +24,29 @@ def testgraph(request):
 	return render_to_response("graphviz.html", {'ecnumber':0, 'read':0, 'dbentry':'DB1', 'bitscore':30, 'evalue':0.005, 'depth':1, 'hits':10}, context_instance=RequestContext(request) )
 @login_required
 def graphrefresh(request): #make a new JSon, set defaults if needed
-	if request.POST['bitscore'] != '':
-		bitscore = float(request.POST['bitscore'])
-	else :
-		bitscore = 30	  #bitscore default
-	if request.POST['e-value'] != '':
-		evalue = float(request.POST['e-value'])
-	else :
-		evalue = 0.005		#e-value default
-	if request.POST['depth'] != '':
-		depth = float(request.POST['depth'])
-	else :
-		depth = 1		 #depth default
-	if request.POST['hits'] != '':
-		hits = float(request.POST['hits'])
-	else :
-		hits = 10		  #hits default
+	condition_dict = { 'bitscore': 30, 'evalue': 0.005, 'depth': 1, 'hits': 10 }
+	for k in condition_dict.keys():
+		if request.POST[k] != '':
+			condition_dict[k] = request.POST[k]
 	error = ''
 	if (request.POST['ecnumber']=='' and request.POST['read']=='' and request.POST['dbentry']!=''):
 		error = create_json(0, 0, request.POST['dbentry'], bitscore, evalue, depth, hits)
 	elif (request.POST['ecnumber']!='' and request.POST['read']=='' and request.POST['dbentry']==''):
-		try:
 			ec_numbers = EnzymeName.objects.filter(Q(enzyme_name__icontains=request.POST['ecnumber']) | Q(ec_number=request.POST['ecnumber']))
-		except EnzymeName.DoesNotExist:
-				return render(request, 'graphviz.html', {'error_message': 'Enzyme not found'})
-		error = create_json(request.POST['ecnumber'], 0, 0, bitscore, evalue, depth, hits)
+			num_results = len(ec_numbers)
+			if num_results == 0: return render(request, 'graphviz.html', condition_dict + {'error_message': 'Enzyme not found'})
+			elif num_results == 1: error = create_json(ec_numbers[0].ec_number, 0, 0, bitscore, evalue, depth, hits)
+			else: return render(request, graphviz.html, condition_dict + {'enzyme_list': ec_numbers})
+
 	elif (request.POST['ecnumber']=='' and request.POST['read']!='' and request.POST['dbentry']==''):
 		error = create_json(0, request.POST['read'], 0, bitscore, evalue, depth, hits)
 	else:
-		return render(request, 'graphviz.html', {
+		return render(request, 'graphviz.html', condition_dict + {
 			'error_message': "Error: You can only enter one of the following: Enzyme, DB entry id, Read id.",
 		})
 	if (error == 'error_no_children'):
-		return render(request, 'graphviz.html', {
+		return render(request, 'graphviz.html', condition_dict + {
 			'error_message': "Error: No data found, input different values.",
 		})
 	#c = Context ({ecnumber:request.POST['ecnumber'], read:request.POST['read'], dbentry:request.POST['dbentry'], bitscore:request.POST['bitscore'], evalue:request.POST['e-value'], depth:request.POST['depth'], hits:request.POST['hits']})
-	return render(request, "graphviz.html", {'ecnumber':request.POST['ecnumber'], 'read':request.POST['read'], 'dbentry':request.POST['dbentry'], 'bitscore':request.POST['bitscore'], 'evalue':request.POST['e-value'], 'depth':request.POST['depth'], 'hits':request.POST['hits']})
+	return render(request, "graphviz.html", condition_dict)
