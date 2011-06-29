fef336b68fbd7f346c294c1114fbf73c1a530aac
Lasse Tyrväinen <sundokan@gmail.com>
2011-06-29 13:39:51 +0300

    Added some tests

--- src/geovision/text_to_db/tests/test_graph_JSON.py
+++ src/geovision/text_to_db/tests/test_graph_JSON.py
@@ -52,6 +52,8 @@ class Test_graph_JSONTestCase(unittest.TestCase):
 
 	def test_query_to_json_init_1(self):
 		self.assertRaises(Exception, QueryToJSON, db_entry="DB1", read="R1")
+
+	def test_query_to_json_init_2(self):
 		q = QueryToJSON(db_entry = "DB2")
 		self.assertEquals(q.startnode, DbEntry.objects.get(db_id = "DB2"))
 		self.assertEquals(q.startpoint, NodeId("DB2", "db_entry"))
@@ -59,6 +61,18 @@ class Test_graph_JSONTestCase(unittest.TestCase):
 		self.assertEquals(q.bitscore_limit, 0)
 		self.assertEquals(q.depth_limit, 2)
 		self.assertEquals(q.max_amount, 5)
+
+	def test_query_to_json_init_3(self):
+		q = QueryToJSON(read = "R1", e_value_limit=0.002, bitscore_limit=300)
+		self.assertEquals(q.startnode, Read.objects.get(read_id = "R1"))
+		self.assertEquals(q.startpoint, NodeId("R1", "read"))
+		self.assertEquals(q.e_value_limit, 0.002)
+		self.assertEquals(q.bitscore_limit, 300)
+		self.assertEquals(q.depth_limit, 2)
+		self.assertEquals(q.max_amount, 5)
+
+	def test_query_to_json_graphbuild_1(self):
+		q = QueryToJSON(db_entry = "DB1")
 
 if __name__ == '__main__':
     unittest.main()
