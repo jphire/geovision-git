ee635fc4c7b1fcd1edccccff3b48528435ca5482
Janne Laukkanen <janne.laukkanen83@gmail.com>
2011-07-11 00:59:43 +0300

    debug in try on enzyme query

--- src/geovision/text_to_db/graph_JSON.py
+++ src/geovision/text_to_db/graph_JSON.py
@@ -191,14 +191,12 @@ class QueryToJSON:
 	def make_enzyme_query(self, param = None):
 
 		db_list = []
-		str=""
 		db_query = DbUniprotEcs.objects.filter(ec = param.dict["id"])
 		for line in db_query:
 			if line.db_id not in db_list:
-				str = str + str(line.db_id.db_id) + "  "
 #				node = DbEntry.objects.get(db_id = line.db_id.db_id)
 				db_list.append(line.db_id)
-		raise Exception("test1 exception" + str)
+		raise Exception("test1 exception")
 		db_entrys = Blast.objects.filter(db_entry__in = db_list)
 		return db_entrys
 
