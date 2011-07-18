674619b70b62306090e3a58e318e81c7136a41fa
tulilaulu <tulilaulu@gmail.com>
2011-07-18 12:56:59 +0300

    urls

--- src/geovision/urls.py
+++ src/geovision/urls.py
@@ -22,7 +22,5 @@ urlpatterns = patterns('',
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 	(r'^admin/?', include(admin.site.urls)),
-	(r'^site/?', include(admin.site.urls)),
-	(r'^auth/?', include(admin.site.urls)),
-	(r'', 'userdb.views.loginpage'), #everything else regirects to login for now
+	#(r'', 'userdb.views.loginpage'), #everything else regirects to login for now
 )
