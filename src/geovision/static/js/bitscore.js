fee2c24c8dfaadd19a7a02d7c9ff195628b0e998
tulilaulu <tulilaulu@gmail.com>
2011-07-28 14:49:33 +0300

    filter commented

--- src/geovision/static/js/bitscore.js
+++ src/geovision/static/js/bitscore.js
@@ -49,14 +49,15 @@ function filter(bitscore, masterbitscore) {
 	if (!(bitscore > 0)) { /*bitscores must make sense*/
 		$('#filtererror').html("Not a valid bitscore.<br/>");
 	}
-	if (bitscore > 0 && bitscore < masterbitscore){
+	if (bitscore > 0 && bitscore < masterbitscore){ /*read below ^^*/
 		$('#filtererror').html("You cannot filter by bitscores lower than the bitscore you used to search the database.<br/>");
 	}
 	else {
-		$('#load').html("Filtering...");
+		$('#load').html("Filtering..."); /*tell the user its loading*/
+		/*contract everything but the root. Uses its own contract, not the main one*/
+		rgraph.op.filterContract(rgraph.graph.getNode(rgraph.root), {type: "replot"}); 
 
-		rgraph.op.filterContract(rgraph.graph.getNode(rgraph.root), {type: "replot"});
-
+		/*go through everything recursively and show nodes that have enough bitscore or are tagged*/
 		rgraph.graph.getNode(rgraph.root).eachAdjacency(function helper(edge){
 			var target;
 			if (edge.nodeTo._depth > edge.nodeFrom._depth) {
@@ -76,13 +77,15 @@ function filter(bitscore, masterbitscore) {
 				})
 			}
 		})
+		/*refresh the graph. Should this be animated?*/
 		rgraph.op.viz.refresh();
 
-		$('#load').html("");
-		$('#filtererror').html("");
+		$('#load').html(""); /*take loading away*/
+		$('#filtererror').html(""); /*did not get an error*/
 		return;
 	}
 }
+/*special version of contract function used only by the filter*/
 function filterContract(node, opt) {
 	var viz = this.viz;
 	opt = $jit.util.merge(this.options, viz.config, opt || {}, {
