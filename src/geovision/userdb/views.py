d6c65b1f1300090dddcdc7a02568f2ab3c704904
tulilaulu <tulilaulu@gmail.com>
2011-08-03 15:42:01 +0300

    comments on userdb-views

--- src/geovision/userdb/views.py
+++ src/geovision/userdb/views.py
@@ -11,21 +11,24 @@ from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
 import json
 
+#redirects authenticated users straight in and shows the login form for the rest
 def loginpage(request):
 	if request.user.is_authenticated():
 		return redirect('/graphrefresh')
 	else:
 		return render_to_response("login.html", { }, context_instance=RequestContext(request) )
+#shows the page with the registering form
 def register(request):
 	return render_to_response("register.html", { }, context_instance=RequestContext(request) )
 
+#registers a new user, is called from the registering page
 def registering(request):
 	datatable = [request.POST['username'], request.POST['email'], request.POST['password1'], request.POST['password2']]
-	for data in datatable:
+	for data in datatable: #checks to see that all fields have data
 		if (len(data) < 1):
 			return render_to_response('register.html', {
 			'error_message': "Error: All fields must be filled.",
-		}, context_instance=RequestContext(request)) #error for for not filling all fields!
+		}, context_instance=RequestContext(request)) #error message for for not filling all fields!
 	if (cmp(request.POST['password1'], request.POST['password2']) != 0 ):
 		return render_to_response('register.html', {
 			'error_message': "Error: Passwords did not match.",
@@ -43,31 +46,37 @@ def registering(request):
 		user.save()
 		return render_to_response('login.html', {
 				'error_message': "Account succesfully created.",
-		}, context_instance=RequestContext(request)) 
+		}, context_instance=RequestContext(request))
+		#new account created - note: it must be activated via the admin panel to be fully functional
+
+#logs a user in, is called from the login page
 def logging_in(request):
 	username = request.POST['username']
 	password = request.POST['password']
-	user = authenticate(username=username, password=password)
+	user = authenticate(username=username, password=password) #djangos auth
 	if user is not None:
-		if user.is_active:
+		if user.is_active: #user has to be active to log in
 			login(request, user)
 			return redirect('/graphrefresh')
 		else:
-			return render_to_response('login.html', {
+			return render_to_response('login.html', { #error for a not activated account
 					'error_message': "Account is not active.",
 					}, context_instance=RequestContext(request))
 	else:
-		return render_to_response('login.html', {
+		return render_to_response('login.html', { #login for incorrect username and/or password
 			'error_message': "Username or password was incorrect.",
 		}, context_instance=RequestContext(request))
 
+#logs the user out. Is called from the logout -button
 def logging_out(request):
 	logout(request)
 	return redirect('/')
 
+#shows the about page
 def about(request):
 	return render_to_response("about.html", { }, context_instance=RequestContext(request) )
 
+#saves the users settings
 @login_required
 def savesettings(request):
 		profile = request.user.get_profile()
@@ -86,6 +95,7 @@ def savesettings(request):
 			subtype = request.POST['animationsubtype']
 			
 			settings = json.dumps({'settings': {'canvaswidth': canvas_x, 'canvasheight': canvas_y}, 'animationsettings': {'type': type, 'duration': duration, 'subtype': subtype, 'transition': transition}})
+			#makes a json out of the settings and saves it
 			profile.settings = settings
 			profile.save()
 			return HttpResponseRedirect('/graphrefresh?settingsmessage=settings saved')
@@ -95,6 +105,8 @@ def savesettings(request):
 			return HttpResponseRedirect('/graphrefresh?settingsmessage=defaults restored')
 		else:
 			return HttpResponseRedirect('/graphrefresh?settingsmessage=error')
+		#in all cases, redirects back to the graph
+#saves the graph the user was looking at
 @login_required
 def save_view(request):
 	profile = request.user.get_profile()
