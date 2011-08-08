7fb9b11885dd10820ff74d3c5a14b5f14dcae84e
Tuomas Tynkkynen <dezgeg@gmail.com>
2011-08-08 15:56:38 +0300

    Fixed tests.

--- src/geovision/text_to_db/tests/blast_parser_tests.py
+++ src/geovision/text_to_db/tests/blast_parser_tests.py
@@ -3,11 +3,10 @@ from django.utils import unittest
 
 from geovision.settings import TEST_FILE_PATH, RUNNING_ON_USERS
 from geovision.text_to_db.blast_parser import create_blast
-from geovision.viz.models import Blast
+from geovision.viz.models import Blast, BlastExtra
 from geovision.viz.models import DbEntry
 from geovision.viz.models import Read
 from nose.tools import raises
-#from nose.plugins.skip.Skip import SkipTest
 
 @unittest.skipUnless(RUNNING_ON_USERS, "Blast parser tests require a PostgreSQL database")
 class BlastParserTests(unittest.TestCase):
@@ -36,20 +35,23 @@ class BlastParserTests(unittest.TestCase):
 		
 
 		results = Blast.objects.all()
+		extras = BlastExtra.objects.all()
+		self.assertEqual(len(results), len(extras))
 		for i in range(len(results)):
 			self.assertEqual(results[i].read, self.read)
 			self.assertEqual(results[i].database_name, self.dbe[i].source_file)
 			self.assertEqual(results[i].db_entry, self.dbe[i])
-			self.assertEqual(results[i].pident, float(self.strings[i][2]))
-			self.assertEqual(results[i].length, Decimal(self.strings[i][3]))
-			self.assertEqual(results[i].mismatch, Decimal(self.strings[i][4]))
-			self.assertEqual(results[i].gapopen, Decimal(self.strings[i][5]))
-			self.assertEqual(results[i].qstart, Decimal(self.strings[i][6]))
-			self.assertEqual(results[i].qend, Decimal(self.strings[i][7]))
-			self.assertEqual(results[i].sstart, Decimal(self.strings[i][8]))
-			self.assertEqual(results[i].send, Decimal(self.strings[i][9]))
 			self.assertEqual(str(results[i].error_value), self.strings[i][10])
 			self.assertEqual(results[i].bitscore, float(self.strings[i][11]))
+
+			self.assertEqual(extras[i].pident, float(self.strings[i][2]))
+			self.assertEqual(extras[i].length, Decimal(self.strings[i][3]))
+			self.assertEqual(extras[i].mismatch, Decimal(self.strings[i][4]))
+			self.assertEqual(extras[i].gapopen, Decimal(self.strings[i][5]))
+			self.assertEqual(extras[i].qstart, Decimal(self.strings[i][6]))
+			self.assertEqual(extras[i].qend, Decimal(self.strings[i][7]))
+			self.assertEqual(extras[i].sstart, Decimal(self.strings[i][8]))
+			self.assertEqual(extras[i].send, Decimal(self.strings[i][9]))
 
 
 #	@raises(Read.DoesNotExist)
