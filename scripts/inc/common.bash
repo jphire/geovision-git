6685bc74b6b65131ec45fc10b9d1ac96a551a4eb
Tuomas Tynkkynen <dezgeg@gmail.com>
2011-08-15 21:18:49 +0300

    Added lighttpd setup script & tested start/stop scripts, made setup scripts more configurable.

--- scripts/inc/common.bash
+++ scripts/inc/common.bash
@@ -1,6 +1,8 @@
 # Add utility scripts as well as locally installed programs to path.
-PATH=.:$HOME/.local/bin/:$PATH
+PATH=.:./inc/:$HOME/.local/bin/:$HOME/.local/sbin/:$PATH
 PATH=$PATH:/usr/local/pgsql/bin/:/usr/lib/postgresql/8.4/bin/
+
+source `dirname $0`/conf/defaults.conf
 statusecho() {
 	echo -e "\033[1;32m>>>>>" "$@" "\033[0m"
 }
