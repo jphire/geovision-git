67bdf569d56efbf6d15567eea76fdec4c03809e7
Janne Laukkanen <janne.laukkanen83@gmail.com>
2011-08-15 14:49:16 +0300

    asynchronous events tests added

--- src/geovision/static/js_tests.js
+++ src/geovision/static/js_tests.js
@@ -1,16 +1,8 @@
 /**
  * This file includes qunit tests for all javascript code not in external libraries.
- *
  */
 
 $(document).ready(function(){
-		module("Basic Unit Test");
-		test("Sample test", function()
-		{
-			expect(2);
-			equals((4/2), 2, 'Expected 2 as the result, result was: ' + (4/2));
-			equals(divide(4,2), 2, 'Expected 2 as the result, result was: ' + divide(4,2));
-		});
 
 		module("moreinfo.js");
 		test("Alignment test", function()
@@ -21,37 +13,49 @@ $(document).ready(function(){
 		});
 
 		module("graphviz.js");
-		asyncTest("Graph creation test", function()
+		 /* Asynchronous testing required when using ajax!*/
+		asyncTest("Graph creation and initial query result test", function()
 		{
-			expect(1);
+			expect(5);
 			initGraph();
-//			var rgraph = new RGraph(Config);
-//			var json;
-			
-			$.getJSON('/graphjson', query, function(json) {
-					rgraph.loadJSON(prepareJSON(json), query.root || 0);
-			});
+//			$.getJSON('/graphjson', query, function(json) {
+//					rgraph.loadJSON(prepareJSON(json), query.root || 0);
+//			});
 			setTimeout(function(){
 				ok(rgraph.busy==false, "rgraph.busy: " + rgraph.busy);
-				ok(rgraph.config.levelDistance==Config.levelDistance, "rgraph.levelDistance: " + rgraph.levelDistance);
+				ok(rgraph.config.levelDistance==Config.levelDistance, "rgraph.levelDistance: " + rgraph.config.levelDistance);
 				ok(rgraph.config.Node.alpha==Config.Node.alpha, "rgraph.config.Node.alpha: " + rgraph.config.Node.alpha);
 				ok(rgraph.config.Edge.dim==Config.Edge.dim, "rgraph.config.Edge.dim: " + rgraph.config.Edge.dim);
 				ok(rgraph.json.length == 5, "rgraph.json.length was: " + rgraph.json.length);
+				ok(rgraph.root=="R1", "rgraph.root was: " + rgraph.root);
 				start();
 			}, 1000);
 		});
 
 		module("events.js");
-		test("Events test", function()
+		asyncTest("Events test", function()
 		{
-			expect(2);
+			expect(3);
 			initGraph();
 			var node = rgraph.graph.getNode(rgraph.root);
-			Config.Events.onMouseEnter(node);
 			var cursorStyle = rgraph.canvas.getElement().style.cursor;
-			equals(cursorStyle, 'pointer', 'Expected pointer as the result, result was: ' + cursorStyle);
+			Config.Events.onMouseEnter(node);
+			setTimeout(function(){
+				ok(cursorStyle=='pointer', 'Expected cursorStyle to be pointer, instead it was: ' + rgraph.canvas.getElement().style.cursor);
+				start();
+			}, 1000);
+
 			Config.Events.onMouseLeave(node);
 			cursorStyle = rgraph.canvas.getElement().style.cursor;
-			equals(cursorStyle, '', 'Expected pointer as the result, result was: ' + cursorStyle);
+			setTimeout(function(){
+				ok(cursorStyle=='', 'Expected cursorStyle to be empty, instead it was: ' + rgraph.canvas.getElement().style.cursor);
+				start();
+			}, 1000);
+			
+			Config.Events.onClick(node);
+			setTimeout(function(){
+				ok('DB5' in rgraph.graph.nodes, 'DB5 was not in the graph');
+				start();
+			}, 1000);
 		});
 	});
