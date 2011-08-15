c1e7a9099f6b418267a8218ec4796abbd4a8eae5
Janne Laukkanen <janne.laukkanen83@gmail.com>
2011-08-15 23:33:58 +0300

    debugging

--- src/geovision/static/js_tests.js
+++ src/geovision/static/js_tests.js
@@ -7,14 +7,25 @@ $(document).ready(function(){
 		module("moreinfo.js");
 		asyncTest("Alignment test", function()
 		{
-			expect(1);
+			expect(9);
 			initGraph();
+
+			setTimeout(function(){
+				ok(rgraph.busy==false, "rgraph.busy: " + rgraph.busy);
+				ok(rgraph.config.levelDistance==Config.levelDistance, "rgraph.levelDistance: " + rgraph.config.levelDistance);
+				ok(rgraph.config.Node.alpha==Config.Node.alpha, "rgraph.config.Node.alpha: " + rgraph.config.Node.alpha);
+				ok(rgraph.config.Edge.dim==Config.Edge.dim, "rgraph.config.Edge.dim: " + rgraph.config.Edge.dim);
+				ok(rgraph.json.length == 5, "rgraph.json.length was: " + rgraph.json.length);
+				ok(rgraph.root=="R1", "rgraph.root was: " + rgraph.root);
+				start();
+			}, 1000);
+
 			setTimeout(function(){
 				alignmentfunction(26092180, 'A1A835', 'GDQ9FB102FUTYO');
 				ok($('span.alignmentpart')[0].innerHTML != '', "Alignmentfunction has data: " + ($('span.alignmentpart')[0].innerHTML!=''));
 				ok(closealign.innerHTML=="Close", "Alignmentfunction has close button:" + (closealign.innerHTML=="Close"));
 				start();
-			}, 2000);
+			}, 1000);
 
 			setTimeout(function(){
 				closealignment(closealign);
