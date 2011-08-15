6b0640ffe1b0371c6950bfdcf4b46b91930ceaab
Tuomas Tynkkynen <dezgeg@gmail.com>
2011-08-15 18:42:05 +0300

    Added local postgres setup/start/stop scripts.

--- scripts/inc/common.bash
+++ scripts/inc/common.bash
@@ -1,4 +1,13 @@
 # Add utility scripts as well as locally installed programs to path.
 PATH=.:$HOME/.local/bin/:$PATH
-statusecho() { echo -e "\033[1;32m>>>>>" "$@" "\033[0m" }
-errorecho() { echo -e "\033[1;31m*****" "$@" "\033[0m" }
+PATH=$PATH:/usr/local/pgsql/bin/:/usr/lib/postgresql/8.4/bin/
+statusecho() {
+	echo -e "\033[1;32m>>>>>" "$@" "\033[0m"
+}
+errorecho() {
+	echo -e "\033[1;31m*****" "$@" "\033[0m"
+}
+die() {
+	errorecho "$@"
+	exit 2
+}
