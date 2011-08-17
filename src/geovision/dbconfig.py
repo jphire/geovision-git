9786310511bb2bb1fe9722c2cdf752bcec81d28b
Tuomas Tynkkynen <dezgeg@gmail.com>
2011-08-17 10:04:35 +0300

    Changed settings back to current users settings.

--- src/geovision/dbconfig.py
+++ src/geovision/dbconfig.py
@@ -4,16 +4,29 @@ from socket import gethostname
 login_user = os.environ['USER']
 RUNNING_ON_USERS = (gethostname() == 'users') 
 if RUNNING_ON_USERS: 
-	DATABASES = {
-		'default': {
-			'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-			'NAME': 'geovision', # Or path to database file if using sqlite3.
-			'USER': login_user, # Not used with sqlite3.
-			'PASSWORD': '', # Not used with sqlite3.
-			'HOST': '/home/tmtynkky/geovision-postgres/',
-			'PORT': '4394'
+	if login_user != 'tmtynkky':
+		DATABASES = {
+			'default': {
+				'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+				'NAME': 'tkt_gvis', # Or path to database file if using sqlite3.
+				'USER': login_user, # Not used with sqlite3.
+				'PASSWORD': '', # Not used with sqlite3.
+				'HOST': '/home/tkt_gvis/pg_data/',
+				'PORT': '58786'
+			}
 		}
-	}
+	else:
+		DATABASES = {
+			'default': {
+				'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+				'NAME': 'geovision', # Or path to database file if using sqlite3.
+				'USER': login_user, # Not used with sqlite3.
+				'PASSWORD': '', # Not used with sqlite3.
+				'HOST': '/home/tmtynkky/geovision-postgres/',
+				'PORT': '4394'
+			}
+		}
+
 else: # Use a local SQLite database if not on users
 	DATABASES = {
 	'default': {
