48f3939a20116b59ee0b5c4f6d42b5b101d5820f
Tuomas Tynkkynen <dezgeg@gmail.com>
2011-08-17 09:58:17 +0300

    Small improvements.

--- scripts/inc/common.bash
+++ scripts/inc/common.bash
@@ -2,7 +2,7 @@
 PATH=.:./inc/:$HOME/.local/bin/:$HOME/.local/sbin/:$PATH
 PATH=$PATH:/usr/local/pgsql/bin/:/usr/lib/postgresql/8.4/bin/
 
-source `dirname $0`/conf/defaults.conf
+source conf/defaults.conf
 statusecho() {
 	echo -e "\033[1;32m>>>>>" "$@" "\033[0m"
 }
